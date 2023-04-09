f9a6185bb2edb15de3cb203a5e9e4683313d7d4a
brottobhmg <cbx01@hotmail.it>
2023-04-09 10:33:39 +0200

    Create scaper.py

---
+++ scaper.py
@@ -0,0 +1,72 @@
+import json
+import requests
+import time
+
+
+
+blocks_address="https://beta-api.explorer.space/api/blocks/height/"
+tx_address="https://beta-api.explorer.space/api/blocks//coins"
+
+last_block=requests.get("https://xchscan.com/api/blocks?limit=1").json()["blocks"][0]["height"]
+print("Height of blockchain: "+str(last_block))
+
+data=[]
+f=open("chia.json","w")
+f.write("{\n")
+i=0
+while i<last_block:
+
+    try:
+        response=requests.get(blocks_address+str(i))
+    except:
+        time.sleep(10)
+        continue
+    if response.status_code>=400:
+        print("Status code: "+str(response.status_code))
+        time.sleep(10)
+        continue
+    response=json.loads(response.text)
+
+
+    response2=requests.get(tx_address[:-6]+str(i)+tx_address[-6:])
+    if response2.status_code>=400:
+        print("Status code: "+str(response2.status_code))
+        time.sleep(10)
+        continue
+    response2=json.loads(response2.text)
+    response["tx"]=response2["results"]
+
+    f.write("\""+str(i)+"\":"+json.dumps(response)+",")
+    print(str(i))
+    i+=1
+
+
+f.write("\n}")
+f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
